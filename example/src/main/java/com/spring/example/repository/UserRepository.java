555f2b1d88114534496553989334cf0c63d672d9
AryaKrishna <sreearya56@gmail.com>
2025-11-20 11:31:39 +0530

    changed the id type to long

--- example/src/main/java/com/spring/example/repository/UserRepository.java
+++ example/src/main/java/com/spring/example/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.spring.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.spring.example.entity.Users;
 
-public interface UserRepository extends JpaRepository<Users, String>{
+public interface UserRepository extends JpaRepository<Users, Long>{
 
 	Users findByUsername(String username);
 
